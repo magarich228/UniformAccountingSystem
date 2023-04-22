08286268672b57077fb07d9e21a53bf60272225d
Kirill <kirillgroshev52@gmail.com>
2023-04-23 01:59:45 +0300

    Добавление пустых моделей и контекста БД.

---
+++ src/UniformAccountingSystem.Data/UniformAccountingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UniformAccountingSystem.Data
+{
+    public class UniformAccountiongContext : DbContext
+    {
+        public UniformAccountiongContext()
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
