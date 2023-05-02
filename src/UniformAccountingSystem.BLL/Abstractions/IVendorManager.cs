1e6b422a22a999092c60569715017c3bcfc0e766
Kirill <kirillgroshev52@gmail.com>
2023-05-02 22:42:14 +0300

    Реализация VendorManager.

--- src/UniformAccountingSystem.BLL/Abstractions/IVendorManager.cs
+++ src/UniformAccountingSystem.BLL/Abstractions/IVendorManager.cs
@@ -5,9 +5,9 @@ namespace UniformAccountingSystem.BLL.Abstractions
     public interface IVendorManager
     {
         Task<IEnumerable<VendorDto>> GetAllAsync(CancellationToken cancellationToken = default);
-        Task<VendorDto> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-        Task<Guid> AddAsync(VendorDto entity, CancellationToken cancellationToken = default);
-        Task<VendorDto> UpdateAsync(VendorDto vendorDto, CancellationToken cancellationToken = default);
-        Task<VendorDto> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<VendorDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<Guid?> AddAsync(VendorDto entity, CancellationToken cancellationToken = default);
+        Task<VendorDto?> UpdateAsync(VendorDto vendorDto, CancellationToken cancellationToken = default);
+        Task<bool?> DeleteByIdAsync(Guid id, CancellationToken cancellationToken = default);
     }
 }
--- src/UniformAccountingSystem.BLL/Services/VendorManager.cs
+++ src/UniformAccountingSystem.BLL/Services/VendorManager.cs
@@ -1,6 +1,8 @@
-﻿using UniformAccountingSystem.BLL.Abstractions;
+﻿using Microsoft.EntityFrameworkCore;
+using UniformAccountingSystem.BLL.Abstractions;
 using UniformAccountingSystem.BLL.Dtos;
 using UniformAccountingSystem.Data;
+using UniformAccountingSystem.Data.Entities;
 
 namespace UniformAccountingSystem.BLL.Services
 {
@@ -13,29 +15,60 @@ namespace UniformAccountingSystem.BLL.Services
             _db = db;
         }
 
-        public Task<Guid> AddAsync(VendorDto entity, CancellationToken cancellationToken = default)
+        public async Task<IEnumerable<VendorDto>> GetAllAsync(CancellationToken cancellationToken = default) =>
+            await _db.Vendors.Select(v => Mapping.Map<UniformVendor, VendorDto>(v)).ToListAsync(cancellationToken);
+
+        public async Task<VendorDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var vendor = await _db.Vendors.FindAsync(new object[] { id }, cancellationToken: cancellationToken);
+
+            if (vendor == null)
+                return null;
+
+            return Mapping.Map<UniformVendor, VendorDto>(vendor);
         }
 
-        public Task<VendorDto> DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+        public async Task<Guid?> AddAsync(VendorDto entity, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var vendor = Mapping.Map<VendorDto, UniformVendor>(entity);
+
+            if (vendor == null)
+                return null;
+
+            await _db.Vendors.AddAsync(vendor, cancellationToken);
+            var result = await _db.SaveChangesAsync(cancellationToken);
+
+            return (result > 0) ? vendor.Id : null;
         }
 
-        public Task<IEnumerable<VendorDto>> GetAllAsync(CancellationToken cancellationToken = default)
+        public async Task<VendorDto?> DeleteByIdAsync(Guid id, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var vendor = await _db.Vendors.FindAsync(new object[] { id }, cancellationToken: cancellationToken);
+
+            if (vendor == null) 
+                return null;
+
+            _db.Vendors.Remove(vendor);
+
+            return (await _db.SaveChangesAsync(cancellationToken)) > 0;
         }
 
-        public Task<VendorDto> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        public async Task<VendorDto?> UpdateAsync(VendorDto vendorDto, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
-        }
+            var vendor = await _db.Vendors.FindAsync(new object[] { vendorDto.Id }, cancellationToken: cancellationToken);
 
-        public Task<VendorDto> UpdateAsync(VendorDto vendorDto, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
+            if (vendor == null) 
+                return null;
+
+            vendor.Name = vendorDto.Name;
+            vendor.Phone = vendorDto.Phone;
+            vendor.Email = vendorDto.Email;
+            vendor.Description = vendorDto.Description;
+            
+            _db.Vendors.Update(vendor);
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return Mapping.Map<UniformVendor, VendorDto>(vendor);
         }
     }
 }
